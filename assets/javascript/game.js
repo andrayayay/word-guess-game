4a146a610787ab06e66a9a945f96c1e5eedab32e
Andrayayay <andrayayay@gmail.com>
2018-12-22 12:52:53 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,54 @@
+// array 
 
-// Choosing a randon word
-
-var states = ["south Carolina", "wisconsin", "washington", "texas", "minnesota","hawaii"];
-
+var states = ["texas", "wisconsin", "washington", "hawaii", "alaska", "california","minnesota","louisana", "georgia"];
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var gameStarted = false;
-var currentWord;
-var wordAsDashes;
+var letter;
+var currentAnswer;
+var answerDashes;
+var gameStart = false;
 var guessesLeft;
-var lettersGuessed;
-var numWins = 0;
-var numLosses = 0;
-var getNewWord;
-var wordPlace; 
+var lettersGuessed = [];
 var correctGuesses;
-var wordAsArr = [];
+var numWins = 0;
+var createNewWord;
+var stateArrayIndex; 
+var wordsArray = [];
 var dashesArray = [];
 
-function initialize() {
-	gameStarted = true;
-	lettersGuessed = [];
-	correctGuesses = 0;
-	wordPlace = Math.floor(Math.random() * 6);
-	currentWord = states[wordPlace];			//string
-	guessesLeft = 17 - currentWord.length;		//longer words get less guesses
-	wordAsDashes = makeIntoDashes(currentWord);	//string of dashes
-	wordAsArr = currentWord.split('');			//array with letters
-	dashesArray = wordAsDashes.split('');		//array with dashes
-	document.getElementById("currentWord").innerHTML = wordAsDashes;
-	document.getElementById("lettersGuessed").innerHTML = "--";
-	document.getElementById("guessesLeft").innerHTML = guessesLeft;
-}
+// Start game 
+// Press any key to start...
+// Declare variables 
 
-// Make each word into underscores, visually like hangman
-function makeIntoDashes(word) {
+function initialize() {
+	gameStart = true;
+	correctGuesses= 0;
+
+	// generates a random word 
+	stateArrayIndex = Math.floor(Math.random() * 9);
+	currentAnswer = states[stateArrayIndex]
+	console.log(states);
+	console.log(stateArrayIndex);
+	// guesses left equation
+	guessesLeft = 12 
+	console.log(currentAnswer);
+	// Split random word into individual letters and dashes
+	answerDashes = wordIntoDashes(currentAnswer);	//string of dashes
+	wordsArray = currentAnswer.split('');			//breaks down answer into letters
+	dashesArray = answerDashes.split('');	
+	console.log(answerDashes);	//breaks down letters into dashes
+	// Make it show in HTML 
+	document.getElementById("currentAnswer").textContent = answerDashes;
+	document.getElementById("lettersGuessed").textContent = "--";
+	document.getElementById("guessesLeft").textContent = guessesLeft;
+}
+initialize()
+
+// User can press any letter to get started	
+// As a key is clicked, letter will show up from an underscore
+// it will show as 'letterGuessed' or 'correctGuesses'
+// create for loop
+
+function wordIntoDashes(word) {
 	var dashes = "";
 	for (i = 0; i < word.length - 1; i++) {
 		dashes += "_ ";
@@ -42,69 +56,67 @@ function makeIntoDashes(word) {
 	dashes += "_";
 	return dashes;
 }
+// function that controls what happenes when a letter is pressed
+function gameStart(letterTyped) {
+	var letter = letterTyped.toLowerCase();
 
-// Main function that controls what to do with each keystroke
-function playGame(letter) {
-	var letter = letter.toLowerCase();
-
-	// Checks if key is a letter
-	if (alphabet.indexOf(letter) > -1) {
-		if (wordAsArr.indexOf(letter) > -1) {
+	if (alphabet.indexOf(letterTyped) > -1) {
+		if (wordsArray.indexOf(letterTyped) > -1) {
 			correctGuesses++;
-			displayLetter(letter);
+			displayLetter(letterTyped);
+			guessesLeft--;
+			document.getElementById("guessesLeft").innerHTML = guessesLeft;
 		}
 		else {
-			if (lettersGuessed.indexOf(letter) > -1) {
-				return;
-			}
-			else {
-				guessesLeft--;
-				document.getElementById("guessesLeft").innerHTML = guessesLeft;
-				lettersGuessed.push(letter);
-				document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(' ');
-				if (guessesLeft == 0) {
-					alert("Sorry! The correct answer is " + currentWord);
-					initialize();
-					numLosses++;
-					document.getElementById("losses").innerHTML = numLosses;
-				}
-			}
+			lettersGuessed.push(letterTyped)
+			guessesLeft--;
+			document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(' ');
 		}
-	}
+		
+	
+	} 
 }
-
-// Displays letter if it's in word
+// this displays a letter if it's in the answer
 function displayLetter(letter) {
 	// for each char in wordAsDashes, if matches currentWord --> display
-	for (i = 0; i < currentWord.length; i++) {
-		if (letter == wordAsArr[i]) {
+	for (i = 0; i < currentAnswer.length; i++) {
+		if (letter == wordsArray[i]) {
 			dashesArray[i * 2] = letter;
 			console.log(dashesArray);
 		}
 	}
-	document.getElementById("currentWord").innerHTML = dashesArray.join("");
+	document.getElementById("currentAnswer").innerHTML = dashesArray.join("");
 	checkForWin();
 }
 
-// Checks for win by looking for "_"
+
+// If user guesses word correctly, answer (and image will show)
 function checkForWin() {
 	if (dashesArray.indexOf("_") === -1) {
-		document.innerHTML("You got it! The correct answer is " + currentWord);
+		alert("Great job! The answer is " + currentAnswer);
 		numWins++;
-		document.getElementById("wins").innerHTML = numWins;
+		document.getElementById("numWins").innerHTML = numWins;
 		initialize();
 	}
 }
 
 document.onkeyup = function (event) {
-	if (!gameStarted) {
-		document.getElementById("letsPlay").innerHTML = "";
-		initialize();
-		document.getElementById("currentWord").innerHTML = wordAsDashes.split(",");
-		console.log(currentWord);
-		gameStarted = true;
-	}
-	else {
-		playGame(event.key);
-	}
+
+	// if (event.key===lettersGuessed) {
+	// 	document.getElementById("startKey").innerHTML = "";
+	// 	document.getElementById("currentAnswer").innerHTML = answerDashes.split(",");
+	// 	console.log(currentAnswer);
+	// 	gameStart = true;
+	// }
+	// else {
+		gameStart(event.key);
+	// }
 }
+
+
+// Automatically choose another word after game is over
+
+
+
+
+
